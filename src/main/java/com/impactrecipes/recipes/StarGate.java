4ab713277eeb34999b905a9c0f9b5caa2979d436
Alex Grishankov <domian844@gmail.com>
2021-04-01 01:09:54 +0700

    hz fix

--- src/main/java/com/impactrecipes/recipes/StarGate.java
+++ src/main/java/com/impactrecipes/recipes/StarGate.java
@@ -26,26 +26,16 @@ public class StarGate implements Runnable {
     }
 
     private void removeSG() {
-        removeRecipeByOutput(GT_ModHandler.getModItem("SGCraft", "sgChevronUpgrade", 1L, 0), true,
-                false, true);
-        removeRecipeByOutput(GT_ModHandler.getModItem("SGCraft", "stargateRing", 1L, 1), true, false,
-                true);
-        removeRecipeByOutput(GT_ModHandler.getModItem("SGCraft", "stargateRing", 1L, 0), true, false,
-                true);
-        removeRecipeByOutput(GT_ModHandler.getModItem("SGCraft", "stargateBase", 1L, 0), true, false,
-                true);
-        removeRecipeByOutput(GT_ModHandler.getModItem("SGCraft", "stargateController", 1L, 0), true,
-                false, true);
-        removeRecipeByOutput(GT_ModHandler.getModItem("SGCraft", "ic2PowerUnit", 1L, 0), true, false,
-                true);
-        removeRecipeByOutput(GT_ModHandler.getModItem("SGCraft", "sgIrisUpgrade", 1L, 0), true, false,
-                true);
-        removeRecipeByOutput(GT_ModHandler.getModItem("SGCraft", "sgIrisBlade", 1L, 0), true, false,
-                true);
-        removeRecipeByOutput(GT_ModHandler.getModItem("SGCraft", "ocInterface", 1L, 0), true, false,
-                true);
-        removeRecipeByOutput(GT_ModHandler.getModItem("SGCraft", "ic2Capacitor", 1L, 0), true, false,
-                true);
+        removeRecipeByOutput(GT_ModHandler.getModItem("SGCraft", "sgChevronUpgrade", 1L, 0));
+        removeRecipeByOutput(GT_ModHandler.getModItem("SGCraft", "stargateRing", 1L, 1));
+        removeRecipeByOutput(GT_ModHandler.getModItem("SGCraft", "stargateRing", 1L, 0));
+        removeRecipeByOutput(GT_ModHandler.getModItem("SGCraft", "stargateBase", 1L, 0));
+        removeRecipeByOutput(GT_ModHandler.getModItem("SGCraft", "stargateController", 1L, 0));
+        removeRecipeByOutput(GT_ModHandler.getModItem("SGCraft", "ic2PowerUnit", 1L, 0));
+        removeRecipeByOutput(GT_ModHandler.getModItem("SGCraft", "sgIrisUpgrade", 1L, 0));
+        removeRecipeByOutput(GT_ModHandler.getModItem("SGCraft", "sgIrisBlade", 1L, 0));
+        removeRecipeByOutput(GT_ModHandler.getModItem("SGCraft", "ocInterface", 1L, 0));
+        removeRecipeByOutput(GT_ModHandler.getModItem("SGCraft", "ic2Capacitor", 1L, 0));
     }
 
     private void  printer3d() {
