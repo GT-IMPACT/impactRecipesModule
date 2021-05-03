3d1cd1bed59f2cd5c4fefd224205dbb77e487132
Спартак <spartak_korolev@mail.ru>
2021-05-03 17:43:31 +0300

    Change Implosion Compressor recipes

--- src/main/java/com/impactrecipes/recipes/ImpactCore.java
+++ src/main/java/com/impactrecipes/recipes/ImpactCore.java
@@ -1300,90 +1300,90 @@ public class ImpactCore implements Runnable {
     private void implosionCompressor() {
         RA.addImplosionRecipe(CoreItems2.getRecipe(97, 1), 5,
                 GT_ModHandler.getModItem("GalaxySpace", "item.CompressedPlates", 1L, 4),
-                GT_OreDictUnificator.get(OrePrefixes.nugget, Materials.StainlessSteel, 1L));
+                GT_OreDictUnificator.get(OrePrefixes.dust, Materials.StainlessSteel, 1L), new int[]{10000, 1000});
         RA.addImplosionRecipe(GT_OreDictUnificator.get(OrePrefixes.block, Materials.Coal, 1L), 1,
                 GT_ModHandler.getModItem("GalaxySpace", "item.CompressedCoal", 1L, 0),
-                null);
+                GT_OreDictUnificator.get(OrePrefixes.dust, Materials.Ash, 1L), new int[]{10000, 1000});
         RA.addImplosionRecipe(
                 GT_OreDictUnificator.get(OrePrefixes.compressed, Materials.Aluminium, 2L), 1,
                 CoreItems2.getRecipe(176, 1),
-                null);
+                GT_OreDictUnificator.get(OrePrefixes.dust, Materials.Ash, 1L), new int[]{10000, 1000});
         RA.addImplosionRecipe(GT_OreDictUnificator.get(OrePrefixes.compressed, Materials.Bronze, 2L),
                 1, CoreItems2.getRecipe(175, 1),
-                null);
+                GT_OreDictUnificator.get(OrePrefixes.dust, Materials.Ash, 1L), new int[]{10000, 1000});
 
         RA.addImplosionRecipe(CoreItems2.getRecipe(74, 3), 1, CoreItems2.getRecipe(78, 1),
-                null);
+                GT_OreDictUnificator.get(OrePrefixes.dust, Materials.Ash, 1L), new int[]{10000, 1000});
         RA.addImplosionRecipe(CoreItems2.getRecipe(75, 3), 1, CoreItems2.getRecipe(82, 1),
-                null);
+                GT_OreDictUnificator.get(OrePrefixes.dust, Materials.Ash, 1L), new int[]{10000, 1000});
         RA.addImplosionRecipe(CoreItems2.getRecipe(76, 3), 1, CoreItems2.getRecipe(83, 1),
-                null);
+                GT_OreDictUnificator.get(OrePrefixes.dust, Materials.Ash, 1L), new int[]{10000, 1000});
         RA.addImplosionRecipe(GT_ModHandler.getModItem("IC2", "itemPartIridium", 3L), 2,
                 CoreItems2.getRecipe(81, 1),
-                null);
+                GT_OreDictUnificator.get(OrePrefixes.dust, Materials.Ash, 1L), new int[]{10000, 1000});
         RA.addImplosionRecipe(GT_ModHandler.getModItem("GalacticraftMars", "item.null", 2L, 5), 1,
                 CoreItems2.getRecipe(90, 1),
-                null);
+                GT_OreDictUnificator.get(OrePrefixes.dust, Materials.Ash, 1L), new int[]{10000, 1000});
         RA.addImplosionRecipe(CoreItems2.getRecipe(80, 2), 1, CoreItems2.getRecipe(91, 1),
-                null);
+                GT_OreDictUnificator.get(OrePrefixes.dust, Materials.Ash, 1L), new int[]{10000, 1000});
         RA.addImplosionRecipe(CoreItems2.getRecipe(81, 2), 2, CoreItems2.getRecipe(92, 1),
-                null);
+                GT_OreDictUnificator.get(OrePrefixes.dust, Materials.Ash, 1L), new int[]{10000, 1000});
         RA.addImplosionRecipe(
                 GT_OreDictUnificator.get(OrePrefixes.compressed, Materials.MeteoricIron, 2L), 1,
                 CoreItems2.getRecipe(93, 1),
-                null);
+                GT_OreDictUnificator.get(OrePrefixes.dust, Materials.Ash, 1L), new int[]{10000, 1000});
         RA.addImplosionRecipe(CoreItems2.getRecipe(83, 2), 1, CoreItems2.getRecipe(94, 1),
-                null);
+                GT_OreDictUnificator.get(OrePrefixes.dust, Materials.Ash, 1L), new int[]{10000, 1000});
         RA.addImplosionRecipe(CoreItems2.getRecipe(85, 2), 1, CoreItems2.getRecipe(95, 1),
-                null);
+                GT_OreDictUnificator.get(OrePrefixes.dust, Materials.Ash, 1L), new int[]{10000, 1000});
         RA.addImplosionRecipe(
                 GT_OreDictUnificator.get(OrePrefixes.compressed, Materials.Titanium, 2L), 1,
                 CoreItems2.getRecipe(96, 1),
-                null);
+                GT_OreDictUnificator.get(OrePrefixes.dust, Materials.Ash, 1L), new int[]{10000, 1000});
         RA.addImplosionRecipe(ItemList.Ingot_Heavy1.get(1L), 8,
                 GT_ModHandler.getModItem("GalacticraftCore", "item.heavyPlating", 1L),
-                GT_OreDictUnificator.get(OrePrefixes.nugget, Materials.StainlessSteel, 4L));
+                GT_OreDictUnificator.get(OrePrefixes.dust, Materials.StainlessSteel, 1L), new int[]{10000, 5000});
         RA.addImplosionRecipe(ItemList.Ingot_Heavy2.get(1L), 8,
                 GT_ModHandler.getModItem("GalacticraftMars", "item.null", 1L, 3),
-                GT_OreDictUnificator.get(OrePrefixes.nugget, Materials.Titanium, 4L));
+                GT_OreDictUnificator.get(OrePrefixes.dust, Materials.Titanium, 1L), new int[]{10000, 5000});
         RA.addImplosionRecipe(ItemList.Ingot_Heavy3.get(1L), 8,
                 GT_ModHandler.getModItem("GalacticraftMars", "item.itemBasicAsteroids", 1L),
-                GT_OreDictUnificator.get(OrePrefixes.nugget, Materials.TungstenSteel, 4L));
+                GT_OreDictUnificator.get(OrePrefixes.dust, Materials.TungstenSteel, 1L), new int[]{10000, 5000});
         RA.addImplosionRecipe(CoreItems2.getRecipe(16, 1), 8, CoreItems2.getRecipe(11, 1),
-                GT_OreDictUnificator.get(OrePrefixes.nugget, Materials.Chrome, 4L));
+                GT_OreDictUnificator.get(OrePrefixes.dust, Materials.Chrome, 1L), new int[]{10000, 5000});
         RA.addImplosionRecipe(CoreItems2.getRecipe(17, 1), 8, CoreItems2.getRecipe(12, 1),
-                GT_OreDictUnificator.get(OrePrefixes.nugget, Materials.Iridium, 4L));
+                GT_OreDictUnificator.get(OrePrefixes.dust, Materials.Iridium, 1L), new int[]{10000, 5000});
         RA.addImplosionRecipe(CoreItems2.getRecipe(18, 1), 8, CoreItems2.getRecipe(13, 1),
-                GT_OreDictUnificator.get(OrePrefixes.nugget, Materials.Osmium, 4L));
+                GT_OreDictUnificator.get(OrePrefixes.dust, Materials.Osmium, 1L), new int[]{10000, 5000});
         RA.addImplosionRecipe(CoreItems2.getRecipe(19, 1), 8, CoreItems2.getRecipe(14, 1),
-                GT_OreDictUnificator.get(OrePrefixes.nugget, Materials.Neutronium, 4L));
+                GT_OreDictUnificator.get(OrePrefixes.dust, Materials.Neutronium, 1L), new int[]{10000, 5000});
         RA.addImplosionRecipe(CoreItems2.getRecipe(20, 1), 8, CoreItems2.getRecipe(15, 1),
-                GT_OreDictUnificator.get(OrePrefixes.nugget, Materials.Phoenixite, 4L));
+                GT_OreDictUnificator.get(OrePrefixes.dust, Materials.Phoenixite, 1L), new int[]{10000, 5000});
         RA.addImplosionRecipe(GT_OreDictUnificator.get(OrePrefixes.plateTriple, Materials.Lead, 1L),
                 1, CoreItems2.getRecipe(171, 1),
-                null);
+                GT_OreDictUnificator.get(OrePrefixes.dust, Materials.Ash, 1L), new int[]{10000, 1000});
         RA.addImplosionRecipe(GT_OreDictUnificator.get(OrePrefixes.plateTriple, Materials.Nickel, 1L),
                 1, GT_ModHandler.getModItem("GalaxySpace", "item.CompressedPlates", 1L, 2),
-                null);
+                GT_OreDictUnificator.get(OrePrefixes.dust, Materials.Ash, 1L), new int[]{10000, 1000});
         RA.addImplosionRecipe(
                 GT_OreDictUnificator.get(OrePrefixes.plateTriple, Materials.Oriharukon, 1L), 1,
                 CoreItems2.getRecipe(173, 1),
-                null);
+                GT_OreDictUnificator.get(OrePrefixes.dust, Materials.Ash, 1L), new int[]{10000, 1000});
         RA.addImplosionRecipe(
                 GT_OreDictUnificator.get(OrePrefixes.plateTriple, Materials.Platinum, 1L), 1,
                 CoreItems2.getRecipe(172, 1),
-                null);
+                GT_OreDictUnificator.get(OrePrefixes.dust, Materials.Ash, 1L), new int[]{10000, 1000});
         RA.addImplosionRecipe(
                 GT_OreDictUnificator.get(OrePrefixes.plateTriple, Materials.Adamantium, 1L), 1,
                 CoreItems2.getRecipe(174, 1),
-                null);
+                GT_OreDictUnificator.get(OrePrefixes.dust, Materials.Ash, 1L), new int[]{10000, 1000});
         RA.addImplosionRecipe(GT_OreDictUnificator.get(OrePrefixes.plateTriple, Materials.Cobalt, 1L),
                 1, GT_ModHandler.getModItem("GalaxySpace", "item.CompressedPlates", 1L, 0),
-                null);
+                GT_OreDictUnificator.get(OrePrefixes.dust, Materials.Ash, 1L), new int[]{10000, 1000});
 
         RA.addImplosionRecipe(ItemList.Ingot_IridiumAlloy.get(1L), 8,
                 GT_OreDictUnificator.get(OrePrefixes.plateAlloy, Materials.Iridium, 1L),
-                GT_OreDictUnificator.get(OrePrefixes.nugget, Materials.Titanium, 2L));
+                GT_OreDictUnificator.get(OrePrefixes.dust, Materials.Titanium, 1L), new int[]{10000, 1500});
     }
 
     private void formingPress() {
