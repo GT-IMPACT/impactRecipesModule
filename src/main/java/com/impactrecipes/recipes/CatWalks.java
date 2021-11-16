dfd524c6e6f848291bc83987cc653ba7a8c586e0
Спартак <spartak_korolev@mail.ru>
2021-11-16 23:03:47 +0300

    Add alternative recipes for CatWalks
    - add more alternative steels in recipes

--- src/main/java/com/impactrecipes/recipes/CatWalks.java
+++ src/main/java/com/impactrecipes/recipes/CatWalks.java
@@ -69,6 +69,14 @@ public class CatWalks implements Runnable {
         // --- Steel Grate
         GT_ModHandler.addCraftingRecipe(GT_ModHandler.getModItem("catwalks", "steelgrate", 5L), tBitMask,
                 new Object[]{"ISI", "SIS", "IdI", 'I', OrePrefixes.stick.get(Materials.Steel), 'S', OrePrefixes.screw.get(Materials.Steel)});
+        GT_ModHandler.addCraftingRecipe(GT_ModHandler.getModItem("catwalks", "steelgrate", 10L), tBitMask,
+                new Object[]{"ISI", "SIS", "IdI", 'I', OrePrefixes.stick.get(Materials.Birmabright), 'S', OrePrefixes.screw.get(Materials.Birmabright)});
+        GT_ModHandler.addCraftingRecipe(GT_ModHandler.getModItem("catwalks", "steelgrate", 20L), tBitMask,
+                new Object[]{"ISI", "SIS", "IdI", 'I', OrePrefixes.stick.get(Materials.StainlessSteel), 'S', OrePrefixes.screw.get(Materials.StainlessSteel)});
+        GT_ModHandler.addCraftingRecipe(GT_ModHandler.getModItem("catwalks", "steelgrate", 40L), tBitMask,
+                new Object[]{"ISI", "SIS", "IdI", 'I', OrePrefixes.stick.get(Materials.HSLA), 'S', OrePrefixes.screw.get(Materials.HSLA)});
+        GT_ModHandler.addCraftingRecipe(GT_ModHandler.getModItem("catwalks", "steelgrate", 60L), tBitMask,
+                new Object[]{"ISI", "SIS", "IdI", 'I', OrePrefixes.stick.get(Materials.MaragingSteel300), 'S', OrePrefixes.screw.get(Materials.MaragingSteel300)});
         // --- Blow Torch
         GT_ModHandler.addCraftingRecipe(GT_ModHandler.getModItem("catwalks", "blowtorch", 1L), tBitMask,
                 new Object[]{" SF", "dIS", "Iw ", 'F', GT_ModHandler.getModItem("minecraft", "flint_and_steel", 1L),
@@ -78,16 +86,56 @@ public class CatWalks implements Runnable {
     private void assembler() {
         // --- Catwalk Column
         RA.addAssemblerRecipe(new ItemStack[]{
-                GT_OreDictUnificator.get(OrePrefixes.stick, Materials.Steel, 6),
-                GT_ModHandler.getModItem("catwalks", "steelgrate", 4L),
+                GT_OreDictUnificator.get(OrePrefixes.stick, Materials.Steel, 3),
+                GT_ModHandler.getModItem("catwalks", "steelgrate", 2L),
                 GT_Utility.getIntegratedCircuit(2)}, null,
-                GT_ModHandler.getModItem("catwalks", "support_column", 4L), 300, 30);
+                GT_ModHandler.getModItem("catwalks", "support_column", 2L), 150, 16);
+        RA.addAssemblerRecipe(new ItemStack[]{
+                        GT_OreDictUnificator.get(OrePrefixes.stick, Materials.Birmabright, 3),
+                        GT_ModHandler.getModItem("catwalks", "steelgrate", 4L),
+                        GT_Utility.getIntegratedCircuit(2)}, null,
+                GT_ModHandler.getModItem("catwalks", "support_column", 4L), 150, 30);
+        RA.addAssemblerRecipe(new ItemStack[]{
+                        GT_OreDictUnificator.get(OrePrefixes.stick, Materials.StainlessSteel, 3),
+                        GT_ModHandler.getModItem("catwalks", "steelgrate", 8L),
+                        GT_Utility.getIntegratedCircuit(2)}, null,
+                GT_ModHandler.getModItem("catwalks", "support_column", 8L), 150, 48);
+        RA.addAssemblerRecipe(new ItemStack[]{
+                        GT_OreDictUnificator.get(OrePrefixes.stick, Materials.HSLA, 3),
+                        GT_ModHandler.getModItem("catwalks", "steelgrate", 16L),
+                        GT_Utility.getIntegratedCircuit(2)}, null,
+                GT_ModHandler.getModItem("catwalks", "support_column", 16L), 150, 96);
+        RA.addAssemblerRecipe(new ItemStack[]{
+                        GT_OreDictUnificator.get(OrePrefixes.stick, Materials.MaragingSteel300, 3),
+                        GT_ModHandler.getModItem("catwalks", "steelgrate", 32L),
+                        GT_Utility.getIntegratedCircuit(2)}, null,
+                GT_ModHandler.getModItem("catwalks", "support_column", 32L), 150, 120);
         // --- Scaffold
         RA.addAssemblerRecipe(new ItemStack[]{
                 GT_OreDictUnificator.get(OrePrefixes.stick, Materials.Steel, 2),
                 GT_ModHandler.getModItem("catwalks", "steelgrate", 1L),
                 GT_Utility.getIntegratedCircuit(1)}, null,
-                GT_ModHandler.getModItem("catwalks", "scaffold", 2L), 100, 30);
+                GT_ModHandler.getModItem("catwalks", "scaffold", 2L), 100, 16);
+        RA.addAssemblerRecipe(new ItemStack[]{
+                        GT_OreDictUnificator.get(OrePrefixes.stick, Materials.Birmabright, 2),
+                        GT_ModHandler.getModItem("catwalks", "steelgrate", 2L),
+                        GT_Utility.getIntegratedCircuit(1)}, null,
+                GT_ModHandler.getModItem("catwalks", "scaffold", 4L), 120, 30);
+        RA.addAssemblerRecipe(new ItemStack[]{
+                        GT_OreDictUnificator.get(OrePrefixes.stick, Materials.StainlessSteel, 2),
+                        GT_ModHandler.getModItem("catwalks", "steelgrate", 4L),
+                        GT_Utility.getIntegratedCircuit(1)}, null,
+                GT_ModHandler.getModItem("catwalks", "scaffold", 8L), 140, 48);
+        RA.addAssemblerRecipe(new ItemStack[]{
+                        GT_OreDictUnificator.get(OrePrefixes.stick, Materials.HSLA, 2),
+                        GT_ModHandler.getModItem("catwalks", "steelgrate", 8L),
+                        GT_Utility.getIntegratedCircuit(1)}, null,
+                GT_ModHandler.getModItem("catwalks", "scaffold", 16L), 160, 96);
+        RA.addAssemblerRecipe(new ItemStack[]{
+                        GT_OreDictUnificator.get(OrePrefixes.stick, Materials.MaragingSteel300, 2),
+                        GT_ModHandler.getModItem("catwalks", "steelgrate", 16L),
+                        GT_Utility.getIntegratedCircuit(1)}, null,
+                GT_ModHandler.getModItem("catwalks", "scaffold", 32L), 180, 120);
         // --- Caged Ladda
         RA.addAssemblerRecipe(new ItemStack[]{
                 GT_ModHandler.getModItem("catwalks", "steelgrate", 4L),
@@ -105,5 +153,17 @@ public class CatWalks implements Runnable {
         RA.addLaserEngraverRecipe(GT_OreDictUnificator.get(OrePrefixes.stick, Materials.Steel, 2),
                 GT_Utility.copyAmount(0, GT_OreDictUnificator.get(OrePrefixes.lens, Materials.Glass, 1)),
                 GT_ModHandler.getModItem("catwalks", "steelgrate", 5L), 80, 16, false);
+        RA.addLaserEngraverRecipe(GT_OreDictUnificator.get(OrePrefixes.stick, Materials.Birmabright, 2),
+                GT_Utility.copyAmount(0, GT_OreDictUnificator.get(OrePrefixes.lens, Materials.Glass, 1)),
+                GT_ModHandler.getModItem("catwalks", "steelgrate", 10L), 100, 24, false);
+        RA.addLaserEngraverRecipe(GT_OreDictUnificator.get(OrePrefixes.stick, Materials.StainlessSteel, 2),
+                GT_Utility.copyAmount(0, GT_OreDictUnificator.get(OrePrefixes.lens, Materials.Glass, 1)),
+                GT_ModHandler.getModItem("catwalks", "steelgrate", 20L), 120, 48, false);
+        RA.addLaserEngraverRecipe(GT_OreDictUnificator.get(OrePrefixes.stick, Materials.HSLA, 2),
+                GT_Utility.copyAmount(0, GT_OreDictUnificator.get(OrePrefixes.lens, Materials.Glass, 1)),
+                GT_ModHandler.getModItem("catwalks", "steelgrate", 40L), 140, 96, false);
+        RA.addLaserEngraverRecipe(GT_OreDictUnificator.get(OrePrefixes.stick, Materials.MaragingSteel300, 2),
+                GT_Utility.copyAmount(0, GT_OreDictUnificator.get(OrePrefixes.lens, Materials.Glass, 1)),
+                GT_ModHandler.getModItem("catwalks", "steelgrate", 60L), 160, 120, false);
     }
 }
