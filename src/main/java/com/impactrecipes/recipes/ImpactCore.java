25e15cc8e42187681e188ad0d16e8fb3efa4b687
Спартак <spartak_korolev@mail.ru>
2022-01-05 18:38:01 +0300

    Fix Unboxing recipe of SolarBlocks

--- src/main/java/com/impactrecipes/recipes/ImpactCore.java
+++ src/main/java/com/impactrecipes/recipes/ImpactCore.java
@@ -399,31 +399,31 @@ public class ImpactCore implements Runnable {
 	}
 	
 	private void unboxing() {
-		RA.addUnboxingRecipe(GT_ModHandler.getModItem("impact", "BlockAdvSolarPanel", 1L, 0),
+		RA.addUnboxingRecipe(GT_ItemList.Solar_ULV.get(1),
 				ItemList.Cover_SolarPanel_8V.get(1L), GT_ItemList.ULVRobotArm.get(1L), 800, 1
 		);
-		RA.addUnboxingRecipe(GT_ModHandler.getModItem("impact", "BlockAdvSolarPanel", 1L, 1),
+		RA.addUnboxingRecipe(GT_ItemList.Solar_LV.get(1),
 				ItemList.Cover_SolarPanel_LV.get(1L), ItemList.Robot_Arm_LV.get(1L), 1000, 2
 		);
-		RA.addUnboxingRecipe(GT_ModHandler.getModItem("impact", "BlockAdvSolarPanel", 1L, 2),
+		RA.addUnboxingRecipe(GT_ItemList.Solar_MV.get(1),
 				ItemList.Cover_SolarPanel_MV.get(1L), ItemList.Robot_Arm_MV.get(1L), 1200, 4
 		);
-		RA.addUnboxingRecipe(GT_ModHandler.getModItem("impact", "BlockAdvSolarPanel", 1L, 3),
+		RA.addUnboxingRecipe(GT_ItemList.Solar_HV.get(1),
 				ItemList.Cover_SolarPanel_HV.get(1L), ItemList.Robot_Arm_HV.get(1L), 1400, 16
 		);
-		RA.addUnboxingRecipe(GT_ModHandler.getModItem("impact", "BlockAdvSolarPanel", 1L, 4),
+		RA.addUnboxingRecipe(GT_ItemList.Solar_EV.get(1),
 				ItemList.Cover_SolarPanel_EV.get(1L), ItemList.Robot_Arm_EV.get(1L), 1600, 64
 		);
-		RA.addUnboxingRecipe(GT_ModHandler.getModItem("impact", "BlockAdvSolarPanel", 1L, 5),
+		RA.addUnboxingRecipe(GT_ItemList.Solar_IV.get(1),
 				ItemList.Cover_SolarPanel_IV.get(1L), ItemList.Robot_Arm_IV.get(1L), 1800, 256
 		);
-		RA.addUnboxingRecipe(GT_ModHandler.getModItem("impact", "BlockAdvSolarPanel", 1L, 6),
+		RA.addUnboxingRecipe(GT_ItemList.Solar_LuV.get(1),
 				ItemList.Cover_SolarPanel_LuV.get(1L), ItemList.Robot_Arm_LuV.get(1L), 2000, 1024
 		);
-		RA.addUnboxingRecipe(GT_ModHandler.getModItem("impact", "BlockAdvSolarPanel", 1L, 7),
+		RA.addUnboxingRecipe(GT_ItemList.Solar_ZPM.get(1),
 				ItemList.Cover_SolarPanel_ZPM.get(1L), ItemList.Robot_Arm_ZPM.get(1L), 2200, 4096
 		);
-		RA.addUnboxingRecipe(GT_ModHandler.getModItem("impact", "BlockAdvSolarPanel", 1L, 8),
+		RA.addUnboxingRecipe(GT_ItemList.Solar_UV.get(1),
 				ItemList.Cover_SolarPanel_UV.get(1L), ItemList.Robot_Arm_UV.get(1L), 2400, 7680
 		);
 		
