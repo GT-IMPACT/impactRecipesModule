697ea2f726092b3939a6b68e98ff5d0033697370
Спартак <spartak_korolev@mail.ru>
2023-12-27 16:19:56 +0300

    Fix quantity

--- src/main/java/com/impactrecipes/recipes/Gregtech.java
+++ src/main/java/com/impactrecipes/recipes/Gregtech.java
@@ -1194,7 +1194,7 @@ public class Gregtech implements Runnable {
 		// --- Dark Ash
 		RA.addMixerRecipe(GT_OreDictUnificator.get(OrePrefixes.dust, Materials.Carbon, 1L),
                 GT_OreDictUnificator.get(OrePrefixes.dust, Materials.Ash, 1L), GT_Utility.getIntegratedCircuit(1), GT_Values.NI, GT_Values.NF,
-                GT_Values.NF, GT_OreDictUnificator.get(OrePrefixes.dust, Materials.DarkAsh, 1L), 200, 7);
+                GT_Values.NF, GT_OreDictUnificator.get(OrePrefixes.dust, Materials.DarkAsh, 2L), 200, 7);
 
     }
 
