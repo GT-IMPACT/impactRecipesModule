820b365b6ac527dd5bfbec1a1230852e4bf5287c
Спартак <spartak_korolev@mail.ru>
2024-05-15 09:38:03 +0300

    Added alt recipe for ChunkLoader

--- src/main/java/com/impactrecipes/recipes/ChickenChunks.java
+++ src/main/java/com/impactrecipes/recipes/ChickenChunks.java
@@ -4,7 +4,11 @@ import gregtech.api.enums.GT_Values;
 import gregtech.api.enums.Materials;
 import gregtech.api.enums.OrePrefixes;
 import gregtech.api.util.GT_ModHandler;
+import gregtech.api.util.GT_OreDictUnificator;
+import gregtech.api.util.GT_Utility;
+import net.minecraft.item.ItemStack;
 
+import static gregtech.api.enums.GT_Values.RA;
 import static gregtech.api.util.GT_ModHandler.removeRecipeByOutput;
 
 public class ChickenChunks implements Runnable {
@@ -17,8 +21,14 @@ public class ChickenChunks implements Runnable {
         GT_ModHandler.addCraftingRecipe(GT_ModHandler.getModItem("ChickenChunks", "chickenChunkLoader", 1L, 0), tBitMask, new Object[]{" h ", "BPB", "PFP", 'B', OrePrefixes.bolt.get(Materials.Iron), 'P', OrePrefixes.plate.get(Materials.Gold), 'F', OrePrefixes.frameGt.get(Materials.Iron)});
         GT_ModHandler.addCraftingRecipe(GT_ModHandler.getModItem("ChickenChunks", "chickenChunkLoader", 9L, 1), tBitMask, new Object[]{"sC", 'C', GT_ModHandler.getModItem("ChickenChunks", "chickenChunkLoader", 1L, 0)});
 
-        GT_Values.RA.addCutterRecipe(GT_ModHandler.getModItem("ChickenChunks", "chickenChunkLoader", 1L, 0),
+        RA.addCutterRecipe(GT_ModHandler.getModItem("ChickenChunks", "chickenChunkLoader", 1L, 0),
                 GT_ModHandler.getModItem("ChickenChunks", "chickenChunkLoader", 9L, 1), GT_Values.NI, 50, 8);
+
+        RA.addAssemblerRecipe(new ItemStack[]{
+                        GT_OreDictUnificator.get(OrePrefixes.frameGt, Materials.Iron, 1),
+                        GT_OreDictUnificator.get(OrePrefixes.plate, Materials.Gold, 3),
+                        GT_Utility.getIntegratedCircuit(2)}, null,
+                GT_ModHandler.getModItem("ChickenChunks", "chickenChunkLoader", 1L, 0), 50, 12);
     }
 
     private void removeChickens() {
